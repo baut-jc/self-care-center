4b25aef607957fa6da074ede6f3ed5fb969bf79d
Jocelle Bautista <jcllebautista@gmail.com>
2022-09-11 18:49:08 -0400

    add new array

--- main.js
+++ main.js
@@ -33,6 +33,10 @@ mantras = [
     'I am the sky, the rest is weather.'
 ];
 
+faveMessages = [
+
+]
+
 // //Query Selectors for functionality
 var affirmationRadioButton = document.getElementById('affirmation')
 var mantraRadioButton = document.getElementById('mantra')
@@ -41,25 +45,11 @@ var displayMessage = document.querySelector('.random-message')
 var meditationIcon = document.getElementById('yoga-icon')
 var manifestButton = document.querySelector('.manifest-button')
 var savedButton = document.querySelector('.saved-button')
-var radioOptions = document.querySelectorAll('.option')
 
 // //Event Listeners
-// -event listener for random(function) "affirmation" radio button
-// affirmationRadioButton.addEventListener('click', affirmationsMessages)
-// -event listener for random(function) "mantra" radio button
-// mantraRadioButton.addEventListener('click', mantrasMessages)
-// -event listener to invoke radio buttons and receive message button
-// submitButton.addEventListener('click', hideYoganIcon)
-submitButton.addEventListener('click', hideYogaIcon)
-// -event listener when submit button is clicked
-//**create an event listener that will invoke affirmation&mantra button when clicking receiving message  */
-//**remove the yoga-icon when messages are being posted */
-//** radio button functionality */
-// for(radio in radios) {
-//     radios[radio].onclick = function() {
-//         alert(this.value);
-//     }
-// }
+submitButton.addEventListener('click', receiveMessageDisplay)
+manifestButton.addEventListener('click', saveFaves)
+savedButton.addEventListener('click', viewSavedFaves)
 
 //Functions
 //* to get random string from array.
@@ -69,42 +59,36 @@ function getRandomIndex(array) {
     return display
 }
 
-// //1. Create random affirmations.
+//1. Create random affirmations.
 function affirmationsMessages() {
-    displayMessage.innerText = getRandomIndex(affirmations)
+    displayMessage.innerText += getRandomIndex(affirmations)
 }
 //2. Create random mantras function.
 function mantrasMessages() {
     displayMessage.innerText = getRandomIndex(mantras)
 }
-//3. Create function that will invoke display for random string of array based on array var.
-// function displayMessage() {
-//     console.log(displayRandomMantras())
-// }
-// function submitButton() {
-//     for (affirmationRadioButton of radio)
-// }
-// for(affirmationsMessages in affirmationRadioButton) {
-//     //     radios[radio].onclick = function() {
-//     //         alert(this.value);
-//     //     }
-//     // }
-
-//4. Create function that will invoke random affirmation/mantra when submit button is invoked through invoking other functions affirmation/mantra.
-//5. Hide icon when submit button is clicked.
-function hideYogaIcon() {
+//3. Hide icon when submit button is clicked. Invoke random strings from different radio arrays.
+function receiveMessageDisplay() {
     meditationIcon.classList.add('hidden')
-    if (affirmationRadioButton.checked) {
+    if (affirmationRadioButton) {
+        console.log(affirmationRadioButton)
         affirmationsMessages()
-    } else if (mantraRadioButton.checked) {
+    } else if (mantraRadioButton) {
         mantrasMessages()
     }
 }
-//1. save favorite message
-//2. push message to array
-//3. create save button
-//4. create view saved button
-//5. create delete function
+//1. Push messages to new Array
+function saveFaves() {
+    faveMessages.push(displayMessage.innerText)
+        return faveMessages.shift()
+ }
+//2. Style and InnerText/HTML to show page.
+function viewSavedFaves() {
+    
+    
+
+}
+//3. create delete function
 // function saveFave() {
 //     covers.push(userCover.value)
 //     titles.push(userTitle.value)
